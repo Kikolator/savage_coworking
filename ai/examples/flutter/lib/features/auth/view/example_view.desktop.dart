497208b4ce46ad5266aa95e400caa95c1762f03e
Nic Pijpers <nicholas@kikolator.dev>
2025-12-01 18:59:52 +0100

    Add layout builder example files.

---
+++ ai/examples/flutter/lib/features/auth/view/example_view.desktop.dart
@@ -0,0 +1,10 @@
+/**
+class ExampleViewDesktop extends StatelessWidget {
+  const ExampleViewDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+*/
